6d933ba2fa093e2323a2e12f8467327572d201a3
greg-hugo <hugo.gregory5@gmail.com>
2022-04-30 14:11:02 +0700

    Reconfigured PlayerJob Classes

---
+++ Project_001/src/PlayerJob/Mage.java
@@ -0,0 +1,71 @@
+package PlayerJob;
+
+public class Mage extends Player{
+	
+	public Mage() {
+		strength = 20;
+		agility = 20;
+		intellect = 70;
+		
+		hp = (strength * 3) + agility; 
+		mp = intellect * 2;
+		defense = (strength * 2) + 30;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getMp() {
+		return mp;
+	}
+
+	public void setMp(int mp) {
+		this.mp = mp;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getAgility() {
+		return agility;
+	}
+
+	public void setAgility(int agility) {
+		this.agility = agility;
+	}
+
+	public int getIntellect() {
+		return intellect;
+	}
+
+	public void setIntellect(int intellect) {
+		this.intellect = intellect;
+	}
+	
+}
