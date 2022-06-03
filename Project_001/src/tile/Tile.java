3c68a19be7da3f4b402396631794d407bc2134a2
Vincentius Loanka Sinaga <70961951+XiaoLongEr8@users.noreply.github.com>
2022-06-03 14:26:29 +0700

    Revisi

--- Project_001/src/tile/Tile.java
+++ Project_001/src/tile/Tile.java
@@ -1,11 +1,9 @@
 package tile;
 
-import java.awt.image.BufferedImage;
-
 public class Tile {
 
-	public BufferedImage image;
-	public boolean collision = false;
-	
+	public Tile() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- Project_001/src/tile/TileManager.java
+++ Project_001/src/tile/TileManager.java
@@ -1,35 +1,9 @@
 package tile;
 
-import javax.imageio.ImageIO;
-
-import Menu.GamePanel;
-
 public class TileManager {
 
-	GamePanel gp;
-	Tile[] tile;
-	
-	public TileManager(GamePanel gp) {
-		
-		this.gp = gp;
-		
-		tile = new Tile[10];
-		
-		getTileImage();
-		
+	public TileManager() {
+		// TODO Auto-generated constructor stub
 	}
-	
-	public void getTileImage() {
-		
-		try {
-			
-			tile[0] = new Tile();
-			tile[0].image = ImageIO.read(getClass().getResource("/tiles"))
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
+
 }
