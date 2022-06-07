3d765a544a893ea338435dee405ad96e55f6dd86
Vincentius Loanka Sinaga <70961951+XiaoLongEr8@users.noreply.github.com>
2022-06-07 15:41:58 +0700

    refactor: Map generate + fix: Collision Event
    (~) Update Tile Asset
    (+) Fitur wold map and camera
    (~) Refactoring Map Generate
    (~) Fixing Bug at Collision

--- Project_001/src/Menu/CollisionChecker.java
+++ Project_001/src/Menu/CollisionChecker.java
@@ -15,12 +15,52 @@ public class CollisionChecker {
 		int playerLeftWorldX = player.worldX + player.solidArea.x;
 		int playerRightWorldX = player.worldX + player.solidArea.x + player.solidArea.width;
 		int playerTopWorldY = player.worldY + player.solidArea.y;
-		int playerBottomWorldX = player.worldY + player.solidArea.y + player.solidArea.height;
+		int playerBottomWorldY = player.worldY + player.solidArea.y + player.solidArea.height;
 		
 		int playerLeftCol = playerLeftWorldX / gp.tileSize;
 		int playerRightCol = playerRightWorldX / gp.tileSize;
 		int playerTopRow = playerTopWorldY / gp.tileSize;
-		int playerbuttomRow = playerTopWorldY / gp.tileSize;
+		int playerBottomRow = playerBottomWorldY / gp.tileSize;
+		
+		int tileNum1, tileNum2;
+		
+		if(player.direction.equalsIgnoreCase("up")) {
+			
+			playerTopRow = (playerTopWorldY - player.speed) / gp.tileSize;
+			tileNum1 = gp.tileM.mapTileNum[playerLeftCol][playerTopRow];
+			tileNum2 = gp.tileM.mapTileNum[playerRightCol][playerTopRow];
+			
+			if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true) {
+				player.collisionOn = true;
+			}
+		}
+		else if(player.direction.equalsIgnoreCase("down")){
+			playerBottomRow = (playerBottomWorldY + player.speed) / gp.tileSize;
+			tileNum1 = gp.tileM.mapTileNum[playerLeftCol][playerBottomRow];
+			tileNum2 = gp.tileM.mapTileNum[playerRightCol][playerBottomRow];
+			
+			if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true) {
+				player.collisionOn = true;
+			}
+		}
+		else if(player.direction.equalsIgnoreCase("left")){
+			playerLeftCol = (playerLeftWorldX - player.speed) / gp.tileSize;
+			tileNum1 = gp.tileM.mapTileNum[playerLeftCol][playerTopRow];
+			tileNum2 = gp.tileM.mapTileNum[playerLeftCol][playerBottomRow];
+			
+			if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true) {
+				player.collisionOn = true;
+			}
+		}
+		else if(player.direction.equalsIgnoreCase("right")){
+			playerRightCol= (playerRightWorldX + player.speed) / gp.tileSize;
+			tileNum1 = gp.tileM.mapTileNum[playerRightCol][playerTopRow];
+			tileNum2 = gp.tileM.mapTileNum[playerRightCol][playerBottomRow];
+			
+			if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true) {
+				player.collisionOn = true;
+			}
+		}
 		
 	}
 
