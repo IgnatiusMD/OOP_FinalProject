e5e3d570bc50e5c8f0151d4549094177b4a22f8b
Vincentius Loanka Sinaga <70961951+XiaoLongEr8@users.noreply.github.com>
2022-06-11 09:36:19 +0700

    Cleaning Code

--- Project_001/src/Menu/Map1.java
+++ Project_001/src/Menu/Map1.java
@@ -5,35 +5,33 @@ import java.awt.CardLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import controller.*;
-
 public class Map1 {
-	
+
 	public JFrame window = new JFrame();
 	public CardLayout cardLayout = new CardLayout();
 	public JPanel panel = new JPanel(cardLayout);
-	
+
 	public GamePanel gamePanel = new GamePanel(this);
-	
+
 	public Map1() {
 		runMap();
 	}
-	
+
 	private void runMap() {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.setTitle("Last Tantasy");
-		
+
 		window.add(gamePanel);
-		
+
 		window.pack();
-		
+
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
-		
+
 		gamePanel.setUpGame();
-		
+
 		gamePanel.startGameThread();
-		
+
 	}
 }
